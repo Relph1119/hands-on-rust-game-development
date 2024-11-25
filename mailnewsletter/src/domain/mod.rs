ba9823981e1ca8f1ea45b91c3c3e126598c7519b
Relph1119 <huruifeng1202@163.com>
2024-11-25 16:18:03 +0800

    Add 学习6.12节：邮箱验证

---
+++ mailnewsletter/src/domain/mod.rs
@@ -0,0 +1,7 @@
+mod subscriber_name;
+mod subscriber_email;
+mod new_subscriber;
+
+pub use new_subscriber::NewSubscriber;
+pub use subscriber_name::SubscriberName;
+pub use subscriber_email::SubscriberEmail;
