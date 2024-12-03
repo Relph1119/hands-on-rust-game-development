bc6259abc78094b8558e3184a3bae3b18aa1faf0
Relph1119 <huruifeng1202@163.com>
2024-12-03 23:09:50 +0800

    Add 学习第11.9节：使用事务隔离，解决并发请求问题

---
+++ mailnewsletter/migrations/20241203142634_relax_null_checks_on_idempotency.sql
@@ -0,0 +1,3 @@
+alter table idempotency alter column response_status_code drop not null;
+alter table idempotency alter column response_body drop not null;
+alter table idempotency alter column response_headers drop not null;
--- mailnewsletter/src/idempotency/persistence.rs
+++ mailnewsletter/src/idempotency/persistence.rs
@@ -2,15 +2,8 @@ use crate::idempotency::IdempotencyKey;
 use actix_web::body::to_bytes;
 use actix_web::http::StatusCode;
 use actix_web::HttpResponse;
-use sqlx::postgres::PgHasArrayType;
-use sqlx::PgPool;
+use sqlx::{Executor, PgPool, Postgres, Transaction};
 use uuid::Uuid;
-
-impl PgHasArrayType for HeaderPairRecord {
-    fn array_type_info() -> sqlx::postgres::PgTypeInfo {
-        sqlx::postgres::PgTypeInfo::with_name("_header_pair")
-    }
-}
 
 #[derive(Debug, sqlx::Type)]
 #[sqlx(type_name = "header_pair")]
@@ -28,9 +21,9 @@ pub async fn get_saved_response(
     let saved_response = sqlx::query!(
         r#"
         SELECT
-            response_status_code,
-            response_headers as "response_headers: Vec<HeaderPairRecord>",
-            response_body
+            response_status_code as "response_status_code!",
+            response_headers as "response_headers!: Vec<HeaderPairRecord>",
+            response_body as "response_body!"
         FROM idempotency
         WHERE user_id = $1 AND idempotency_key = $2
         "#,
@@ -53,7 +46,7 @@ pub async fn get_saved_response(
 }
 
 pub async fn save_response(
-    pool: &PgPool,
+    mut transaction: Transaction<'static, Postgres>,
     idempotency_key: &IdempotencyKey,
     user_id: Uuid,
     http_response: HttpResponse,
@@ -71,19 +64,62 @@ pub async fn save_response(
         h
     };
 
-    sqlx::query_unchecked!(
+    transaction.execute(sqlx::query_unchecked!(
         r#"
-        INSERT INTO idempotency (user_id, idempotency_key, response_status_code, response_headers, response_body, created_at)
-        VALUES ($1, $2, $3, $4, $5, now())
+        UPDATE idempotency
+        SET
+            response_status_code = $3,
+            response_headers = $4,
+            response_body = $5
+        WHERE
+            user_id = $1 AND
+            idempotency_key = $2
         "#,
         user_id,
         idempotency_key.as_ref(),
         status_code,
         headers,
-        body.as_ref(),
-    ).execute(pool)
-        .await?;
+        body.as_ref()
+    ))
+    .await?;
+    transaction.commit().await?;
 
     let http_response = response_head.set_body(body).map_into_boxed_body();
     Ok(http_response)
+}
+
+#[allow(clippy::large_enum_variant)]
+pub enum NextAction {
+    StartProcessing(Transaction<'static, Postgres>),
+    ReturnSavedResponse(HttpResponse),
+}
+
+pub async fn try_processing(
+    pool: &PgPool,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+) -> Result<NextAction, anyhow::Error> {
+    let mut transaction = pool.begin().await?;
+    let query = sqlx::query!(
+        r#"
+        INSERT INTO idempotency (
+            user_id,
+            idempotency_key,
+            created_at
+        )
+        VALUES ($1, $2, now())
+        ON CONFLICT DO NOTHING
+        "#,
+        user_id,
+        idempotency_key.as_ref()
+    );
+    let n_inserted_rows = transaction.execute(query).await?.rows_affected();
+    if n_inserted_rows > 0 {
+        Ok(NextAction::StartProcessing(transaction))
+    } else {
+        let saved_response = get_saved_response(pool, idempotency_key, user_id)
+            .await?
+            .ok_or_else(|| anyhow::anyhow!("We expected a saved response, we didn't find it"))?;
+        Ok(NextAction::ReturnSavedResponse(saved_response))
+    }
 }
